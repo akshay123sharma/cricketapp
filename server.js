4d47e565cb4f658a10a9d9169f21ea1e75358a30
unknown <ak981615091@gmail.com>
2024-02-14 22:29:37 +0530

    test

--- server.js
+++ server.js
@@ -18,9 +18,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
 // const io = require("socket.io")(http);
- app.use(cors({
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(cors({
   origin: '*'
 }));
+
+
 const io = require('socket.io')(http,{
   cors: {
        origin: "*"
